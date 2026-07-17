87ea07e211876a59380934b6ea252b9e35e61dba
Ith Chanakijkamjorn <151436940+IthChanakijkamjorn@users.noreply.github.com>
2026-07-17 13:33:33 +0700

    Remove specSection from schema: tabName optional, rows directly under tab

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,7 +1,6 @@
 import product from './product'
 import specRow from './specRow'
-import specSection from './specSection'
 import specTab from './specTab'
 import project from './project'
 
-export const schemaTypes = [specRow, specSection, specTab, product, project]
+export const schemaTypes = [specRow, specTab, product, project]
